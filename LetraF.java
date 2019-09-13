0aaaaa8fccfe20cdd84b5acc68fe1301e4c54044
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2019-09-13 20:37:15 -0300

    Update LetraF.java

--- LetraF.java
+++ LetraF.java
@@ -1,16 +1,18 @@
-/*package questao01;
-public class LetraF {
-    public static void main(String[] args) {	
-        Pilha pilha = new Pilha(4);
-        
-        for (int i = 1; i < 5; i++) {
-            pilha.inserir(i);
-        }
-              System.out.println(pilha);
-              for (int i = 1; i < 2; i++) {
-                    pilha.remover();
-        }
-    pilha.inserir(100);
-        System.out.print(pilha); 
-      }
-} */
+package questão01;
+public class Questão01 {
+    public static void main(String[] args) {
+    Pilha p = new Pilha(4);
+     Pilha p2 = new Pilha(4);
+     p.inserir(1);
+     p.inserir(2);
+     p.inserir(3);
+     p.inserir(4);
+      int aux =p.remover();
+      int aux2 = p.remover();
+      p.inserir(aux2);
+      p.inserir(aux);
+      p.remover();
+      p.inserir(100);
+        System.out.println(p);
+}
+}
