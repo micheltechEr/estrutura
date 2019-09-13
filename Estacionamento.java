adab5c4e92d49663b84d4a6585ce8c847e8b469c
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2019-09-13 12:29:40 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -1,44 +1,62 @@
 package estacionamento;
 import java.util.Scanner;
 public class Estacionamento {
+     //Os atributos estão visiveis apenas para a classe
+    private final Pilha<String>p; 
+    private final Pilha<String>p2;
+    private int conter;
+    //Inicialização dos atributos
+    public Estacionamento(int max){
+         this.p = new Pilha<>(max);
+         this.p2=new Pilha<>(max);
+         }
+
     public static void main(String[] args) {
-      Pilha p = new Pilha(4);
-      Pilha p2 = new Pilha(4);
-      char mov = 0;
-      int placa =0;
-              Scanner s= new Scanner(System.in);
-      for(int i = 0;i<p.ele.length;i++){
-      do{
-        System.out.println("Informe a movimentação do carro : ");
-  mov=s.next().charAt(0);
-     switch(mov){
-         case 'E':
-                 if(p.estaCheia()){
-                     System.out.println("Estacionamento cheio!");    
-                 }
-                 else {
-                     System.out.println("Por favor,informe a placa");
-                  placa =s.nextInt();
-              p.inserir(placa);
-                     System.out.println(placa+" foi inserido");
-                 }
-            break;
-         case 'S':
-             System.out.println("Informe a posição do carro a ser retirado");
-            int e = s.nextInt();
-           while(i!=e){
-               Object rem = p.remover();
-               p2.inserir(rem);
-           }
-             
-           while(p.inserir(p2.remover())){
-           }
-           System.out.println("Carro da posição "+e+" sendo removido....");
-   }
-       break;
-   }  while(!p.estaCheia());
+        Estacionamento e = new Estacionamento(5);//Instancia do objeto Estacionamento,que tem seus propios metodos e se for requerido,tem acesso aos metodos da Pilha
+        while(e.selec()){} //A estrutura de repetição serve para aposentar o do-while,deixando o código mais limpo.
+        System.out.println(e.impressao()); //Chama o metodo de impressão
+        System.out.println("Foi removido "+e.conter+" vezes"); //Mostra quantas vezes foi removido
     }
-            System.out.println(p);
-}
-}
+    private boolean selec(){
+        Estacionamento e = new Estacionamento(5);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Informe a sua operação: E/S/X");
+        char op = s.nextLine().charAt(0);
+          switch(op){
+            case 'E':
+                System.out.print("Informe a placa do veiculo :"); //Leitura da placa
+                 String placa = s.nextLine();
+                 p.inserir(placa); //Inserindo na Pilha...
+                 System.out.println(placa+" Carro inserido");
+                 return true;
+            case 'S':
+                e.remover(); //Como a função é extensa,usei um metodo,para deixar mais limpo
+                return true;
+            case 'X':
+                 default: System.out.println("Saindo..."); //Caso a opção for X,ele sai do programa
+                 return false;
+        } 
+    } 
+    private void remover(){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Informe a posição: ");
+           int pos = Integer.parseInt(s.nextLine());
+           for(int j = 0;j<=pos;j++){
+               String removido= p.remover();
+                 p2.inserir(removido);
+                 conter++;
+           }
+           for(int j = 0;j<=pos;j++){
+           String placaRem = p.remover();
+              while(p.inserir(p2.remover())){}
+              p.remover();
+              System.out.println("O carro "+placaRem+" foi removido!");
+           break;
+           }
+    }
+    public boolean impressao(){
+        System.out.println(p);
+         return true;
+    }
+    //Código sujeito a modificações futuras.
 
