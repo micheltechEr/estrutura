820f3e18be6761e98954662fc57c6bdc1b01d0e8
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2019-09-13 21:08:26 -0300

    Update LetraF.java

--- LetraF.java
+++ LetraF.java
@@ -3,16 +3,18 @@ public class Questão01 {
     public static void main(String[] args) {
     Pilha p = new Pilha(4);
      Pilha p2 = new Pilha(4);
+     Pilha p = new Pilha(4);
+     Pilha p2 = new Pilha(4);
      p.inserir(1);
      p.inserir(2);
      p.inserir(3);
      p.inserir(4);
-      int aux =p.remover();
-      int aux2 = p.remover();
-      p.inserir(aux2);
-      p.inserir(aux);
-      p.remover();
-      p.inserir(100);
+     int aux =p.remover();
+     p2.inserir(aux);
+     p2.inserir(2);
+     p2.inserir(1);
+     p.inserir(100);
+     p.inserir(p2.remover());
         System.out.println(p);
-}
+   }
 }
