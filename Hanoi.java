50bad68faaeaf0c01a973cd7e0c9731fbc22fea2
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2019-09-16 11:11:51 -0300

    Update Hanoi.java

--- Hanoi.java
+++ Hanoi.java
@@ -1,25 +1,68 @@
 package torrehanoi;
-public class TorreHanoi {
+import java.util.Scanner;
+public class TorredeHanoi {
     public static void main(String[] args) {
-       Pilha p1 = new Pilha<>(8);
-       Pilha <Integer>p2 = new Pilha<>(8);
-       Pilha<Integer> p3 = new Pilha<>(8);
-       p1.inserir(3);
-       p1.inserir(2);
-       p1.inserir(1); 
-          torreHanoi(p1.tamanho, p1, p2, p3);
-    }
-    public static void torreHanoi(int n,Pilha<Integer> p1,Pilha <Integer>p2,Pilha <Integer>p3 ){
-        if(n>0){
-            torreHanoi(n-1, p1, p3, p2);
-         p3.inserir(p1.remover());
-            System.out.println("-----");
-            System.out.println("Original: "+p1);
-            System.out.println("Destino: "+p3);
-            System.out.println("Auxiliar: "+p2);
-            torreHanoi(n-1, p2, p3, p1);
-           
+        Scanner s = new Scanner(System.in);
+        Pilha p = new Pilha(5);
+        Pilha p2 = new Pilha(5);
+        Pilha p3 = new Pilha(5);
+        //As 3 Pilhas representam os Pinos
+        System.out.println("Informe o disco 1");
+          int disco1 = s.nextInt();
+            p.inserir(disco1);
+            System.out.println("Informe o disco 2");
+             int disco2 = s.nextInt();
+            p.inserir(disco2);
+            System.out.println("Informe o disco 3 ");
+            int disco3 = s.nextInt();
+            p.inserir(disco3);
+           //JOGADAS/MOVIMENTOS
+           //2 Movimentos
+          int dest = p.remover(); //Remove o disco1 e guarda na variavel
+          int aux = p.remover(); //Remove o disco2 e guarda na variavel
+          p3.inserir(dest); //Insere na Pilha de destino,o disco1
+          p2.inserir(aux); //Insere na Pilha auxiliar,o disco2
+          //2 Movimentos
+         int aux2= p3.remover(); //Remove o disco1 e guarda em outra variavel
+          p2.inserir(aux2); //Insere o disco1 na pilha auxiliar,com o disco2
+         int dest2 =  p.remover(); //Remove o disco3 da pilha original
+         p3.inserir(dest2); //Insere na pilha destino
+         // 2 Movimentos
+        int original = p2.remover(); // Remove o disco1 e guarda em outra variavel
+        p.inserir(original); //Insere o disco1 na Pilha original
+        int dest3 = p2.remover(); // Remove o disco2 e guarda em outra variavel
+        p3.inserir(dest3); //Insere o disco2 na Pilha destino
+        //1 Movimento,totalizando 7 movimentos
+        int dest4 = p.remover(); //Remove o disco1 da Pilha original,guardando em outra variavel
+        p3.inserir(dest4); //Insere ele na Pilha destino,finalizando a jogada.
+       System.out.println("--------------------------------");
+            System.out.println("Original :" +dest+","+aux+","+dest2);
+            System.out.println("Auxiliar : ");
+            System.out.println("Destino: ");
+        System.out.println("--------------------------------");
+            System.out.println("Original :" +dest);
+            System.out.println("Auxiliar : "+aux);
+            System.out.println("Destino: "+dest2);
+            System.out.println("-------------------------");
+            System.out.println("Original :" +dest);
+            System.out.println("Auxiliar: "+aux+","+dest2);
+            System.out.println("Destino: ");
+        System.out.println("-------------------------");
+            System.out.println("Original :");
+            System.out.println("Auxiliar: "+aux+","+dest2);
+            System.out.println("Destino: "+dest);
+            System.out.println("-------------------------");
+            System.out.println("Original :" +dest2);
+            System.out.println("Auxiliar: "+aux);
+            System.out.println("Destino: "+dest);
+        System.out.println("-------------------------");
+            System.out.println("Original :" +dest2);
+            System.out.println("Auxiliar: ");
+            System.out.println("Destino: "+aux+","+dest);
+        System.out.println("-------------------------");
+            System.out.println("Original :");
+            System.out.println("Auxiliar: ");
+            System.out.println("Destino: "+dest+","+aux+","+dest2);
         }
-        
 }
-}
+
