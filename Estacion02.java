d432df0c2e6cc643f4854c8f62c1d1b75bed2914
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2019-09-27 16:15:28 -0300

    Update Estacion02.java

--- Estacion02.java
+++ Estacion02.java
@@ -1,59 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package estacionamento02;
 import java.util.Scanner;
 public class Estacionamento02 {
-    private final Fila<Carro> f;
-    private final Fila<Carro> f2;
-    public Estacionamento02(int size){
-        this.f = new Fila<>(size);
-        this.f2 = new Fila<>(size);
-    }
     public static void main(String[] args) {
-        Estacionamento02 es  = new Estacionamento02(10);   
-           while(es.selec()){}
+Scanner sc =  new Scanner(System.in);
+Fila<Carro> f = new Fila<>(3); 
+int saida = f.inicio =f.fim=0;
+        System.out.println("Bem vindo");
+    while(true){
+        System.out.println("Informe sua operação: ");
+   char menu = sc.nextLine().charAt(0);
+  if(menu == 'X'){
+      break;
+  }
+  if(menu == 'C'){
+            System.out.println("Digite a placa");
+       String placa = sc.nextLine();
+        Carro c  = new Carro(placa);
+           if(!f.isFull()){
+               f.push(c);
+               System.out.println("Carro "+placa+" inserido");
+           }else{
+               System.out.println("SEM VAGAS!");
+           }
+  }else if(menu =='P'){
+      Fila<Carro> f2 =  new Fila<Carro>(11);     
+      System.out.println("Digite a Placa:");
+  String placa = sc.nextLine();
+  Carro c = procurar(f,placa);
+  if(c!=null){
+       System.out.println("Foi Achado:"+ c.getVezes_contorno());
+  }
+        if(!f2.isEmpty()){
+              Carro c2 = procurar(f,placa); 
+          c2 = f.pop();
+            f2.push(c2);
+               System.out.println("O carro "+c2.getPlaca()+" foi inserido");
+   }else{
+              Carro c2 = procurar(f,placa); 
+            if(c2!=null){
+                System.out.println("Ele foi deslocado "+c.getVezes_contorno());
+            }
+        }
     }
-    public boolean selec(){
-          String placa;
-       Estacionamento02 es  = new Estacionamento02(10);
-        Scanner s = new Scanner(System.in); //Leitura dos dados
-        System.out.println("Informe a operação C/P/X");
-         char op = s.nextLine().charAt(0);
-        switch(op){
-            case 'C':
-   
-   if(!f.isFull()){
-      System.out.println("Digite o numero da placa");
-        placa = s.nextLine();
-       Carro c = new Carro(placa);
-       c.placa = placa;
-    }else{
-       System.out.println("Esta cheio");
-   }
-            case 'P':
-                 Carro c = new Carro(placa);
-                 boolean achou = false;
-                System.out.println("Digite o numero da Placa:");
-String placa2 = s.nextLine();
-                while(!f.isEmpty()) {
-               c.placa = f.pop();
-                if( c.getPlaca().equals(placa2) ) {
+    }
+    }
+    public static Carro procurar( Fila<Carro> fila, String placa) {
+        Fila<Carro> aux = new Fila<Carro>(11);
+        Carro c = null;
+        Boolean achou = false;
+        while(!fila.isEmpty()) {
+                c = fila.pop();
+                if( c.getPlaca().equals(placa) ) {
                         achou = true;
                         break;
                 }else {
-                        f2.push(c);
+                        aux.push(c);
                 }
-                while(!f2.isEmpty()) {
-                Carro c1 = f2.pop();
+        }
+
+        while(!aux.isEmpty()) {
+                Carro c1 = aux.pop();
                 c1.addVezes_contorno();
-                f.push( c1 );
+                fila.push( c1 );
         }
-        }
-                                return true;
-        }
-        return true;
-   }
-     public boolean impressao(){
-        System.out.println("Os carros disponiveis são : \n"+f);
-         return true;
-    }
+
+        if(achou)
+                return c;
+
+        return null;
+
 } 
+    
+}
+
+
+
+
+   
+
+
+
+
+
 
